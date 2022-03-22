6e1bc2bfb28375144baeef4fc16bde77a774674c
ProgramarWe <101956999+ProgramarWe@users.noreply.github.com>
2022-03-22 09:36:48 +0600

    Randomly BG Color Change
    We are going to show JavaScript project in this repository This project is how we can randomly change the background-color again with the help of a button. Here we have used HTML CSS and JavaScript and this is our JS code for this project.

--- randomlyBGcolorChnage.js
+++ randomlyBGcolorChnage.js
@@ -1,1 +1,13 @@
+let BtnOne = document.getElementById('BtnOne');
 
+
+let BtnTwo = document.getElementById('BtnTwo');
+
+let bg = document.getElementById('body')
+
+BtnOne.onclick = () => {
+    let red = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+    bg.style.backgroundColor = `rgb(${red},${green},${blue})`
+}
